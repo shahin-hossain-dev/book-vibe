65c1eb1c42db57861efed3b390089823ac45f779
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-03-26 00:54:10 +0600

    display books data in cardat home page

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaRegStar } from "react-icons/fa6";
+const Book = ({ book }) => {
+  const { image, tags, bookName, author, category, rating } = book;
+  console.log(book);
+  return (
+    <div>
+      <div className="card  p-6 rounded-2xl border h-full shadow-xl">
+        <figure className="p-5 bg-[#F3F3F3] rounded-2xl">
+          <img src={image} alt="Shoes" />
+        </figure>
+        <div className="mt-5 space-y-4 flex-grow ">
+          <div>
+            {tags.map((tag, idx) => (
+              <span
+                key={idx}
+                className="p-2 me-3 font-medium rounded-xl text-[#23BE0A] bg-[#23BE0A0D]"
+              >
+                #{tag}
+              </span>
+            ))}
+          </div>
+          <h3 className="text-2xl font-semibold">{bookName}</h3>
+          <p className="font-semibold text-[#131313CC]">Author: {author}</p>
+          <div className="border-b-2 border-dashed border-[#13131326]"></div>
+          <div className=" flex items-center justify-between">
+            <span>{category}</span>
+            <span className="flex gap-2 items-center">
+              {<FaRegStar className="inline-block" />}
+              <span>{rating}</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("books.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
   return (
     <div className="mt-12 lg:mt-24">
       <h2 className="text-center text-2xl lg:text-4xl font-bold">Books</h2>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6 lg:mt-12">
+        {books.map((book) => (
+          <Book key={book.bookId} book={book}></Book>
+        ))}
+      </div>
     </div>
   );
 };
