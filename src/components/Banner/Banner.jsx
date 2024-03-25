9a180d22ae862a1ad98807ba182d4eaa4fd11c1b
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-03-25 23:27:17 +0600

    banner updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const Banner = () => {
   return (
     <div className="bg-[#1313130D] rounded-3xl">
-      <div className="flex flex-col  lg:flex-row justify-center items-center gap-6 p-6 lg:p-12">
+      <div className="flex flex-col  lg:flex-row justify-around items-center gap-6 p-6 lg:p-12">
         <div className="flex items-center lg:items-start justify-center flex-col">
-          <h1 className="text-3xl lg:text-5xl font-semibold leading-[40px] lg:leading-[60px]	">
+          <h1 className="text-3xl lg:text-6xl font-semibold leading-[40px] lg:leading-[70px]	">
             Books to freshen <br /> up your bookshelf
           </h1>
           <Link to={"/listed-books"}>
-            <button className="btn bg-[#23BE0A] mt-5 text-white px-6 me-3">
+            <button className="btn bg-[#23BE0A] mt-8 text-white px-6 me-3">
               View The List{" "}
             </button>
           </Link>
---
+++ src/components/Books/Books.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Books = () => {
+  return (
+    <div className="mt-12 lg:mt-24">
+      <h2 className="text-center text-2xl lg:text-4xl font-bold">Books</h2>
+    </div>
+  );
+};
+
+export default Books;
