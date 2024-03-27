aca06a6a93f69e012dbf47cad14ff93d60c93c1e
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-03-27 21:17:57 +0600

    Listed book page responsive updated

--- src/components/WishlistBook/WishlistBook.jsx
+++ src/components/WishlistBook/WishlistBook.jsx
@@ -22,24 +22,26 @@ const WishlistBook = ({ book }) => {
   return (
     <div>
       <div className="card flex-col lg:flex-row items-center card-side bg-base-100 shadow-xl border p-6">
-        <div className="bg-[#1313130D] rounded-2xl py-8 px-6 w-[230px] flex justify-center items-center">
+        <div className="bg-[#1313130D] rounded-2xl py-8 px-6 lg:w-[230px] flex justify-center items-center">
           <img src={image} alt="Book" />
         </div>
         {/* card body */}
-        <div className="card-body py-0 space-y-2">
+        <div className="lg:ms-5 px-2 space-y-2">
           <h2 className="card-title">{bookName}</h2>
           <p>By : {author}</p>
           <div className="flex flex-col lg:flex-row lg:items-center gap-3">
             <div className="flex items-center">
               <span className="font-bold">Tags: </span>
-              {tags.map((tag, idx) => (
-                <span
-                  key={idx}
-                  className="p-2 me-3 font-medium rounded-xl text-[#23BE0A] bg-[#23BE0A0D]"
-                >
-                  #{tag}
-                </span>
-              ))}
+              <div className="flex flex-wrap">
+                {tags.map((tag, idx) => (
+                  <span
+                    key={idx}
+                    className="p-2 me-3 font-medium rounded-xl text-[#23BE0A] bg-[#23BE0A0D]"
+                  >
+                    #{tag}
+                  </span>
+                ))}
+              </div>
             </div>
             <div>
               <div className="flex items-center">
@@ -68,7 +70,7 @@ const WishlistBook = ({ book }) => {
             </div>
           </div>
           <hr />
-          <div className="flex gap-3 items-center ">
+          <div className="flex flex-col lg:flex-row gap-3 items-center ">
             <span className="py-2 px-3 rounded-2xl bg-[#328EFF26] text-[#328EFF]">
               Category: {category}
             </span>
